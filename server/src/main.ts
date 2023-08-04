2ce31f1437f23ff7788b74a3ca4fb6ecd2fa6e08
nextremy <99709675+nextremy@users.noreply.github.com>
2023-08-04 11:56:52 -0700

    Implement user router with login and creation

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,9 +2,12 @@ import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import cors from "cors";
 import "dotenv/config";
 import { createContext } from "./context";
+import { userRouter } from "./routers/user";
 import { router } from "./trpc";
 
-const appRouter = router({});
+const appRouter = router({
+  user: userRouter,
+});
 
 export type AppRouter = typeof appRouter;
 
---
+++ server/src/routers/user.ts
@@ -0,0 +1,45 @@
+import { TRPCError } from "@trpc/server";
+import argon2 from "argon2";
+import jwt from "jsonwebtoken";
+import z from "zod";
+import { env } from "../env";
+import { publicProcedure, router } from "../trpc";
+
+export const userRouter = router({
+  login: publicProcedure
+    .input(z.object({ username: z.string(), password: z.string() }))
+    .mutation(async ({ input, ctx }) => {
+      const user = await ctx.db.user.findUnique({
+        where: { username: input.username },
+      });
+      if (!user) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      const passwordsMatch = await argon2.verify(
+        user.passwordHash,
+        input.password
+      );
+      if (!passwordsMatch) {
+        throw new TRPCError({ code: "UNAUTHORIZED" });
+      }
+
+      const token = jwt.sign({ userId: user.id }, env.JWT_SECRET);
+      return token;
+    }),
+  create: publicProcedure
+    .input(z.object({ username: z.string(), password: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const usernameTaken = Boolean(
+        await ctx.db.user.findUnique({ where: { username: input.username } })
+      );
+      if (usernameTaken) {
+        throw new TRPCError({ code: "CONFLICT" });
+      }
+
+      const passwordHash = await argon2.hash(input.password);
+      await ctx.db.user.create({
+        data: { username: input.username, passwordHash },
+      });
+    }),
+});
