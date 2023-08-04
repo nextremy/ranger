c9cfd9cf0be38c1dda7a17fe2e2d2903ef368451
nextremy <99709675+nextremy@users.noreply.github.com>
2023-08-04 16:20:01 -0700

    Implement protected procedures

--- server/src/trpc.ts
+++ server/src/trpc.ts
@@ -1,7 +1,15 @@
-import { initTRPC } from "@trpc/server";
+import { TRPCError, initTRPC } from "@trpc/server";
 import { createContext } from "./context";
 
 const trpc = initTRPC.context<typeof createContext>().create();
 
+const authenticationMiddleware = trpc.middleware(({ ctx, next }) => {
+  if (!ctx.session) {
+    throw new TRPCError({ code: "UNAUTHORIZED" });
+  }
+  return next({ ctx: { session: ctx.session, db: ctx.db } });
+});
+
 export const router = trpc.router;
 export const publicProcedure = trpc.procedure;
+export const protectedProcedure = trpc.procedure.use(authenticationMiddleware);
