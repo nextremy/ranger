0f8076f81be6f83ff27d9213c755568231cbe030
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-07 11:33:51 -0700

    Scaffold post router

--- server/src/main.ts
+++ server/src/main.ts
@@ -2,11 +2,13 @@ import { createHTTPServer } from "@trpc/server/adapters/standalone";
 import cors from "cors";
 import "dotenv/config";
 import { createContext } from "./context";
+import { postRouter } from "./routers/post";
 import { userRouter } from "./routers/user";
 import { router } from "./trpc";
 
 const appRouter = router({
   user: userRouter,
+  post: postRouter,
 });
 
 export type AppRouter = typeof appRouter;
