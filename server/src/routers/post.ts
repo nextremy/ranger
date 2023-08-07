5d9f905f8f204bbbec8f732a051a17186406d3da
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-07 11:49:48 -0700

    Implement post get and create procedures

--- server/src/routers/post.ts
+++ server/src/routers/post.ts
@@ -1,3 +1,38 @@
-import { router } from "../trpc";
+import { TRPCError } from "@trpc/server";
+import z from "zod";
+import { protectedProcedure, publicProcedure, router } from "../trpc";
 
-export const postRouter = router({});
+export const postRouter = router({
+  get: publicProcedure
+    .input(z.object({ id: z.string() }))
+    .query(async ({ input, ctx }) => {
+      const post = await ctx.db.post.findUnique({
+        select: {
+          timestamp: true,
+          text: true,
+          author: { select: { id: true, displayName: true } },
+          _count: { select: { replies: true, reposts: true, likes: true } },
+        },
+        where: { id: input.id },
+      });
+      if (!post) {
+        throw new TRPCError({ code: "NOT_FOUND" });
+      }
+
+      return {
+        timestamp: post.timestamp,
+        text: post.text,
+        author: post.author,
+        replyCount: post._count.replies,
+        repostCount: post._count.reposts,
+        likeCount: post._count.likes,
+      };
+    }),
+  create: protectedProcedure
+    .input(z.object({ text: z.string().min(1).min(300) }))
+    .mutation(async ({ input, ctx }) => {
+      await ctx.db.post.create({
+        data: { text: input.text, authorId: ctx.session.userId },
+      });
+    }),
+});
