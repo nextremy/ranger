59b88750a1087a7034bc1e6ac0e1f00a3d1c949d
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-07 11:22:27 -0700

    Scaffold profile posts/posts & replies lists

--- client/src/routes/profile/index.tsx
+++ client/src/routes/profile/index.tsx
@@ -1,3 +1,4 @@
+import { Tab } from "@headlessui/react";
 import { useParams } from "react-router-dom";
 import { trpc } from "../../trpc";
 import { EditProfileButton } from "./edit-profile-button";
@@ -10,25 +11,48 @@ export function ProfileRoute() {
 
   if (!user) return null;
   return (
-    <div className="p-4">
-      <div className="flex justify-between">
-        <div>
-          <p className="text-lg font-medium leading-tight">
-            {user.displayName}
-          </p>
-          <div className="flex gap-2">
-            <p className="text-gray-600">@{user.username}</p>
-            {user.isFollowingUser ? (
-              <p className="rounded-md bg-gray-200 px-2 font-medium">
-                Follows you
-              </p>
-            ) : null}
+    <>
+      <div className="p-4">
+        <div className="flex justify-between">
+          <div>
+            <p className="text-lg font-medium leading-tight">
+              {user.displayName}
+            </p>
+            <div className="flex gap-2">
+              <p className="text-gray-600">@{user.username}</p>
+              {user.isFollowingUser ? (
+                <p className="rounded-md bg-gray-200 px-2 font-medium">
+                  Follows you
+                </p>
+              ) : null}
+            </div>
           </div>
+          <FollowButton />
+          <UnfollowButton />
+          <EditProfileButton />
         </div>
-        <FollowButton />
-        <UnfollowButton />
-        <EditProfileButton />
       </div>
-    </div>
+      <Tab.Group>
+        <Tab.List className="grid auto-cols-fr grid-flow-col">
+          <Tab className="grid h-12 place-items-center font-semibold text-gray-600 duration-200 hover:bg-gray-200 hover:text-gray-900 ui-selected:text-gray-900">
+            <div>
+              Posts
+              <div className="h-1 rounded-full bg-transparent ui-selected:bg-green-700" />
+            </div>
+          </Tab>
+          <Tab className="grid h-12 place-items-center font-semibold text-gray-600 duration-200 hover:bg-gray-200 hover:text-gray-900 ui-selected:text-gray-900">
+            <div>
+              Posts & replies
+              <div className="h-1 rounded-full bg-transparent ui-selected:bg-green-700" />
+            </div>
+          </Tab>
+        </Tab.List>
+        <Tab.Panels>
+          <Tab.Panel></Tab.Panel>
+          <Tab.Panel></Tab.Panel>
+        </Tab.Panels>
+      </Tab.Group>
+      <div></div>
+    </>
   );
 }
