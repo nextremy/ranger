cac35c774993b3e5cfbbc7f0686f775d7c2290f3
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-07 09:46:01 -0700

    Implement unfollow button

--- client/src/routes/profile/index.tsx
+++ client/src/routes/profile/index.tsx
@@ -1,6 +1,7 @@
 import { useParams } from "react-router-dom";
 import { trpc } from "../../trpc";
 import { FollowButton } from "./follow-button";
+import { UnfollowButton } from "./unfollow-button";
 
 export function ProfileRoute() {
   const params = useParams();
@@ -17,6 +18,7 @@ export function ProfileRoute() {
           <p className="text-gray-600">@{user.username}</p>
         </div>
         <FollowButton />
+        <UnfollowButton />
       </div>
     </div>
   );
---
+++ client/src/routes/profile/unfollow-button.tsx
@@ -0,0 +1,37 @@
+import { CheckIcon } from "@heroicons/react/20/solid";
+import { useParams } from "react-router-dom";
+import { useSession } from "../../hooks/use-session";
+import { trpc } from "../../trpc";
+
+export function UnfollowButton() {
+  const session = useSession();
+  const params = useParams();
+  const context = trpc.useContext();
+  const { data: user } = trpc.user.get.useQuery({ username: params.username! });
+  const { mutate: unfollow } = trpc.user.unfollow.useMutation({
+    onMutate: () => {
+      if (!session) return;
+      context.user.get.setData({ username: session.username }, (data) => {
+        if (!data) return;
+        return { ...data, followingCount: data.followingCount - 1 };
+      });
+      context.user.get.setData({ username: params.username! }, (data) => {
+        if (!data) return;
+        return { ...data, isFollowedByUser: false };
+      });
+    },
+  });
+
+  if (!user || user.id === session?.userId || !user.isFollowedByUser) {
+    return null;
+  }
+  return (
+    <button
+      className="flex h-12 items-center gap-2 rounded-full bg-gray-200 px-6 font-bold duration-200 hover:bg-gray-300"
+      onClick={() => unfollow({ userId: user.id })}
+    >
+      <CheckIcon className="h-5 w-5" />
+      Following
+    </button>
+  );
+}
