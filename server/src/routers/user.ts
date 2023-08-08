8ed36aca4bb32a9c5769cc1e1c72ffc6a735cc5e
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-08 15:16:31 -0700

    Implement list posts procedure

--- server/src/routers/user.ts
+++ server/src/routers/user.ts
@@ -118,4 +118,71 @@ export const userRouter = router({
         where: { id: ctx.session.userId },
       });
     }),
+  listPosts: publicProcedure
+    .input(
+      z.object({
+        username: z.string(),
+        cursor: z.optional(z.string()),
+        includeReplies: z.boolean(),
+      })
+    )
+    .query(async ({ input, ctx }) => {
+      const posts = await ctx.db.post.findMany({
+        select: {
+          id: true,
+          timestamp: true,
+          text: true,
+          author: { select: { id: true, username: true, displayName: true } },
+          replyingTo: {
+            select: {
+              id: true,
+              timestamp: true,
+              text: true,
+              author: {
+                select: { id: true, username: true, displayName: true },
+              },
+              reposts: { where: { userId: ctx.session?.userId } },
+              stars: { where: { userId: ctx.session?.userId } },
+              _count: { select: { replies: true, reposts: true, stars: true } },
+            },
+          },
+          reposts: { where: { userId: ctx.session?.userId } },
+          stars: { where: { userId: ctx.session?.userId } },
+          _count: { select: { replies: true, reposts: true, stars: true } },
+        },
+        where: {
+          author: { username: input.username },
+          replyingToId: input.includeReplies ? undefined : null,
+        },
+        take: 25,
+        orderBy: { timestamp: "desc" },
+        skip: input.cursor ? 1 : 0,
+        cursor: input.cursor ? { id: input.cursor } : undefined,
+      });
+
+      return posts.map((post) => ({
+        id: post.id,
+        timestamp: post.timestamp,
+        text: post.text,
+        author: post.author,
+        replyingTo: post.replyingTo
+          ? {
+              id: post.replyingTo.id,
+              timestamp: post.replyingTo.timestamp,
+              text: post.replyingTo.text,
+              author: post.replyingTo.author,
+              isRepostedByUser: post.replyingTo.reposts.length === 1,
+              isStarredByUser: post.replyingTo.stars.length === 1,
+              replyCount: post._count.replies,
+              repostCount: post._count.reposts,
+              starCount: post._count.stars,
+            }
+          : null,
+        isRepostedByUser: post.reposts.length === 1,
+        isStarredByUser: post.stars.length === 1,
+        replyCount: post._count.replies,
+        repostCount: post._count.reposts,
+        starCount: post._count.stars,
+      }));
+    }),
 });
