9b131527511956cc9f6bf8e621c4e6c77f2b20a5
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-09 14:28:59 -0700

    Refactor reply modal to use new modal component

--- client/src/components/post/reply-button.tsx
+++ client/src/components/post/reply-button.tsx
@@ -1,12 +1,33 @@
 import { ChatBubbleLeftIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useId, useState } from "react";
+import { useForm } from "react-hook-form";
 import { trpc } from "../../trpc";
+import { Form } from "../form";
 import { Modal } from "../modal";
-import { ReplyForm } from "./reply-form";
+
+type Inputs = {
+  postText: string;
+};
 
 export function ReplyButton(props: { postId: string }) {
   const [modalOpen, setModalOpen] = useState(false);
+  const postTextInputId = useId();
+  const context = trpc.useContext();
   const { data: post } = trpc.post.get.useQuery({ id: props.postId });
+  const { mutate: createPost } = trpc.post.create.useMutation({
+    onMutate: () => {
+      context.post.get.setData({ id: props.postId }, (data) => {
+        if (!data) return;
+        return { ...data, replyCount: data.replyCount + 1 };
+      });
+    },
+  });
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm<Inputs>();
 
   if (!post) return null;
   return (
@@ -22,9 +43,49 @@ export function ReplyButton(props: { postId: string }) {
         {post.replyCount}
       </button>
       <Modal onClose={() => setModalOpen(false)} open={modalOpen}>
-        <Modal.Title>Reply to post</Modal.Title>
-        <Modal.CloseButton onClick={() => setModalOpen(false)} />
-        <ReplyForm postId={post.id} setModalOpen={setModalOpen} />
+        <form
+          className="flex flex-col"
+          onSubmit={(event) => {
+            event.preventDefault();
+            void handleSubmit((inputs) => {
+              createPost({ text: inputs.postText, replyingToPostId: post.id });
+              setModalOpen(false);
+              setTimeout(() => reset(), 150);
+            })(event);
+          }}
+        >
+          <Modal.TopBar>
+            <Modal.CloseButton onClick={() => setModalOpen(false)} />
+            <Modal.Title>New reply</Modal.Title>
+            <div className="grow" />
+            <button
+              className="h-12 rounded-full bg-green-700 px-4 font-bold text-gray-100 duration-150 hover:bg-green-800"
+              type="submit"
+            >
+              Post
+            </button>
+          </Modal.TopBar>
+          <label className="sr-only" htmlFor={postTextInputId}>
+            New reply
+          </label>
+          <textarea
+            className="h-48 bg-transparent p-4 placeholder:text-gray-600 "
+            id={postTextInputId}
+            placeholder="What's up?"
+            {...register("postText", {
+              required: "Post cannot be empty",
+              maxLength: {
+                value: 300,
+                message: "Post cannot be longer than 300 characters",
+              },
+            })}
+          />
+          {errors.postText ? (
+            <Form.Error className="text-center">
+              {errors.postText.message}
+            </Form.Error>
+          ) : null}
+        </form>
       </Modal>
     </>
   );
