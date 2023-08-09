aff481685f5649ccd1e06ddbadd867b0fc292666
Remy Wood <99709675+nextremy@users.noreply.github.com>
2023-08-08 17:15:51 -0700

    Implement more actions buttons

--- client/src/components/post/more-actions-button.tsx
+++ client/src/components/post/more-actions-button.tsx
@@ -1,16 +1,55 @@
-import { EllipsisHorizontalIcon } from "@heroicons/react/24/outline";
+import { Menu } from "@headlessui/react";
+import {
+  EllipsisHorizontalIcon,
+  LinkIcon,
+  TrashIcon,
+} from "@heroicons/react/20/solid";
+import { useSession } from "../../hooks/use-session";
 import { trpc } from "../../trpc";
 
 export function MoreActionsButton(props: { postId: string }) {
+  const session = useSession();
+  const context = trpc.useContext();
   const { data: post } = trpc.post.get.useQuery({ id: props.postId });
+  const { mutate: deletePost } = trpc.post.delete.useMutation({
+    onSuccess: () => context.post.get.reset({ id: props.postId }),
+  });
 
   if (!post) return null;
   return (
-    <button
-      className="z-20 grid h-12 w-12 place-items-center rounded-full text-gray-500 duration-150 hover:bg-green-500/10 hover:text-green-500"
-      title="More actions"
-    >
-      <EllipsisHorizontalIcon className="h-5 w-5" />
-    </button>
+    <Menu as="div" className="relative z-20">
+      <Menu.Button
+        className="z-20 grid h-12 w-12 place-items-center rounded-full text-gray-500 duration-200 hover:bg-green-500/10 hover:text-green-500"
+        title="More actions"
+      >
+        <EllipsisHorizontalIcon className="h-5 w-5" />
+      </Menu.Button>
+      <Menu.Items className="absolute bottom-2 right-2 flex w-max flex-col rounded-lg border border-gray-300 bg-gray-100">
+        <Menu.Item as="div">
+          <button
+            className="flex h-12 w-full items-center gap-2 px-4 duration-150 hover:bg-gray-200"
+            onClick={() =>
+              void navigator.clipboard.writeText(
+                `${window.location.origin}/posts/${post.id}`,
+              )
+            }
+          >
+            <LinkIcon className="h-5 w-5" />
+            Copy link
+          </button>
+        </Menu.Item>
+        {post.author.username === session?.username ? (
+          <Menu.Item as="div">
+            <button
+              className="flex h-12 w-full items-center gap-2 px-4 text-red-700 duration-150 hover:bg-gray-200"
+              onClick={() => deletePost({ postId: post.id })}
+            >
+              <TrashIcon className="h-5 w-5" />
+              Delete post
+            </button>
+          </Menu.Item>
+        ) : null}
+      </Menu.Items>
+    </Menu>
   );
 }
--- client/src/routes/post/main-post/more-actions-button.tsx
+++ client/src/routes/post/main-post/more-actions-button.tsx
@@ -1,12 +1,57 @@
-import { EllipsisHorizontalIcon } from "@heroicons/react/20/solid";
+import { Menu } from "@headlessui/react";
+import {
+  EllipsisHorizontalIcon,
+  LinkIcon,
+  TrashIcon,
+} from "@heroicons/react/20/solid";
+import { useParams } from "react-router-dom";
+import { useSession } from "../../../hooks/use-session";
+import { trpc } from "../../../trpc";
 
 export function MoreActionsButton() {
+  const params = useParams();
+  const session = useSession();
+  const context = trpc.useContext();
+  const { data: post } = trpc.post.get.useQuery({ id: params.postId! });
+  const { mutate: deletePost } = trpc.post.delete.useMutation({
+    onSuccess: () => context.post.get.reset({ id: params.postId! }),
+  });
+
+  if (!post) return null;
   return (
-    <button
-      className="grid h-12 w-12 place-items-center rounded-full text-gray-500 duration-200 hover:bg-green-500/10 hover:text-green-500"
-      title="More actions"
-    >
-      <EllipsisHorizontalIcon className="h-5 w-5" />
-    </button>
+    <Menu as="div" className="relative">
+      <Menu.Button
+        className="grid h-12 w-12 place-items-center rounded-full text-gray-500 duration-200 hover:bg-green-500/10 hover:text-green-500"
+        title="More actions"
+      >
+        <EllipsisHorizontalIcon className="h-5 w-5" />
+      </Menu.Button>
+      <Menu.Items className="absolute bottom-2 right-2 flex w-max flex-col rounded-lg border border-gray-300 bg-gray-100">
+        <Menu.Item as="div">
+          <button
+            className="flex h-12 w-full items-center gap-2 px-4 duration-150 hover:bg-gray-200"
+            onClick={() =>
+              void navigator.clipboard.writeText(
+                `${window.location.origin}/posts/${post.id}`,
+              )
+            }
+          >
+            <LinkIcon className="h-5 w-5" />
+            Copy link
+          </button>
+        </Menu.Item>
+        {post.author.username === session?.username ? (
+          <Menu.Item as="div">
+            <button
+              className="flex h-12 w-full items-center gap-2 px-4 text-red-700 duration-150 hover:bg-gray-200"
+              onClick={() => deletePost({ postId: post.id })}
+            >
+              <TrashIcon className="h-5 w-5" />
+              Delete post
+            </button>
+          </Menu.Item>
+        ) : null}
+      </Menu.Items>
+    </Menu>
   );
 }
