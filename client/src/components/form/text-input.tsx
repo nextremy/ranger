bf8220e764090b9d06a64418a129e4d258ff50e9
nextremy <99709675+nextremy@users.noreply.github.com>
2023-08-10 14:50:17 -0700

    Allow text inputs to be fully rounded

--- client/src/components/form/text-input.tsx
+++ client/src/components/form/text-input.tsx
@@ -1,14 +1,16 @@
 import { ComponentPropsWithoutRef, ForwardedRef, forwardRef } from "react";
 
-type Props = ComponentPropsWithoutRef<"input">;
+type Props = { roundedFull?: boolean } & ComponentPropsWithoutRef<"input">;
 
 export const TextInput = forwardRef(function TextInput(
-  { className, ...restProps }: Props,
+  { roundedFull, className, ...restProps }: Props,
   ref: ForwardedRef<HTMLInputElement>,
 ) {
   return (
     <input
-      className={`h-12 rounded-md bg-gray-300 px-4 ${className}`}
+      className={`h-12 bg-gray-300 ${
+        roundedFull ? "rounded-full px-6" : "rounded-md px-4"
+      } ${className}`}
       {...restProps}
       ref={ref}
     />
