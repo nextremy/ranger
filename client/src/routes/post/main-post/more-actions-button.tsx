178180f838e00c1796b684aef9173d087b4da528
nextremy <99709675+nextremy@users.noreply.github.com>
2023-08-10 13:14:17 -0700

    Make more actions button icon vertical dots

--- client/src/routes/post/main-post/more-actions-button.tsx
+++ client/src/routes/post/main-post/more-actions-button.tsx
@@ -1,6 +1,6 @@
 import { Menu, Transition } from "@headlessui/react";
 import {
-  EllipsisHorizontalIcon,
+  EllipsisVerticalIcon,
   LinkIcon,
   TrashIcon,
 } from "@heroicons/react/20/solid";
@@ -25,7 +25,7 @@ export function MoreActionsButton() {
         className="grid h-12 w-12 place-items-center rounded-full text-gray-500 duration-200 hover:bg-green-500/10 hover:text-green-500"
         title="More actions"
       >
-        <EllipsisHorizontalIcon className="h-5 w-5" />
+        <EllipsisVerticalIcon className="h-5 w-5" />
       </Menu.Button>
       <Transition
         as={Fragment}
