9115578cd35147fd3142ab7cd3e68db84d6a76db
nextremy <99709675+nextremy@users.noreply.github.com>
2023-08-10 10:50:55 -0700

    Configure color palette

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -1,10 +1,20 @@
 import headlessUI from "@headlessui/tailwindcss";
+import colors from "tailwindcss/colors";
 import defaultTheme from "tailwindcss/defaultTheme";
 
 /** @type {import("tailwindcss").Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
+    colors: {
+      current: "currentColor",
+      transparent: "transparent",
+      gray: colors.slate,
+      red: colors.red,
+      yellow: colors.yellow,
+      green: colors.emerald,
+      blue: colors.blue,
+    },
     extend: {
       fontFamily: {
         sans: ["Inter", ...defaultTheme.fontFamily.sans],
